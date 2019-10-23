88447675d6ca0b78917c70b17623cec03eeca524
Waleed Almogbel <walmogbel@stratpoint.com>
2019-10-23 14:21:58 +0800

    changes on category field from int to string type

--- cms-api/src/infra/migrations/20191016104235-create-advisories.js
+++ cms-api/src/infra/migrations/20191016104235-create-advisories.js
@@ -10,7 +10,7 @@ module.exports = {
       },
       advisoryId : Sequelize.STRING,
       userId: Sequelize.INTEGER,
-      categoryId : Sequelize.INTEGER,
+      categoryName : Sequelize.STRING,
       title : Sequelize.STRING,
       content : Sequelize.STRING,
       source : Sequelize.STRING,
