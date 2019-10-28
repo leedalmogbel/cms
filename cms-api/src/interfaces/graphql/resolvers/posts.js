94a6bef95ace61a8d1c96471db0201ef0bf35fe4
Waleed Almogbel <walmogbel@stratpoint.com>
2019-10-28 11:23:52 +0800

    refactor advisories and post

--- cms-api/src/interfaces/graphql/resolvers/posts.js
+++ cms-api/src/interfaces/graphql/resolvers/posts.js
@@ -1,24 +1,24 @@
 module.exports = {
   Query: {
-    getPosts: (_, args, { container, res }) => {
+    getPosts: (_, args, { container }) => {
       const operation = container.resolve('ListPosts');
       return operation.execute(args);
     },
-    getPost: (_, args, { container, res }) => {
+    getPost: (_, args, { container }) => {
       const operation = container.resolve('ShowPost');
       return operation.execute(args);
     },
   },
   Mutation: {
-    createPostDraft: (_, args, { container, res, next }) => {
+    createPostDraft: (_, args, { container }) => {
       const operation = container.resolve('CreatePostDraft');
       return operation.execute(args);
     },
-    savePost: (_, args, { container, res, next }) => {
+    savePost: (_, args, { container }) => {
       const operation = container.resolve('SavePost');
       return operation.execute(args);
     },
-    publishPost: (_, args, { container, res, next }) => {
+    publishPost: (_, args, { container }) => {
       const operation = container.resolve('PublishPost');
       return operation.execute(args);
     },
