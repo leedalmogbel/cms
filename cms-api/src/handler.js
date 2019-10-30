715093ebafe683c7e7e8bc0f9aa6945cf01b6031
Michael Raagas <mraagas@stratpoint.com>
2019-10-30 15:58:43 +0800

    smart tag generator serverless function and refactor location autocomplete

--- cms-api/src/handler.js
+++ cms-api/src/handler.js
@@ -1,26 +1,49 @@
-const pg = require('pg'); 
+const fetch = require('node-fetch');
 
-module.exports.index = async (event, context, callback) => {
+const post = (url, body) => new Promise((resolve, reject) => {
+  fetch(url, {
+    method: 'post',
+    body,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      resolve(json);
+    });
+});
 
-    const config = {
-        user: process.env.DB_USERNAME,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        max: 5,
-      };
+module.exports.location = async (event, context, callback) => {
+  const res = await post(
+    'https://vv0j1ovhzj.execute-api.ap-southeast-1.amazonaws.com/dev2/users/location/autocomplete',
+    event.body,
+  );
 
-      const pool = new pg.Pool(config);
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'X-Content-Type-Options': 'nosniff',
+    },
+    body: JSON.stringify(res),
+  };
+};
 
-      const client = await pool.connect();
-      const { rows } = await client.query(`
-      DROP TABLE advisories CASCADE;
-      DROP TABLE categories CASCADE;
-      DROP TABLE tags CASCADE;
-      DROP TABLE posts CASCADE;
-      DROP TABLE users CASCADE; 
-      `);
+module.exports.smartTags = async (event, context, callback) => {
+  const res = await post(
+    'https://gp1g9sn1x9.execute-api.ap-southeast-1.amazonaws.com/hle-staging/api/v1/smart-tags',
+    event.body,
+  );
 
-      console.log("rows");
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'X-Content-Type-Options': 'nosniff',
+    },
+    body: JSON.stringify(res),
+  };
 };
