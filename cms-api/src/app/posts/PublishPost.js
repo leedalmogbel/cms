73bd152a3bfdf704254d23bd82de72a744f134bc
Waleed Almogbel <walmogbel@stratpoint.com>
2019-11-04 14:59:38 +0800

    uncomment some credentials

--- cms-api/src/app/posts/PublishPost.js
+++ cms-api/src/app/posts/PublishPost.js
@@ -7,9 +7,9 @@ class PublishPost extends Operation {
   constructor({ SavePost }) {
     super();
     this.SavePost = SavePost;
-    // this.firehose = new AWS.Firehose({
-    //   apiVersion: '2015-08-04',
-    // });
+    this.firehose = new AWS.Firehose({
+      apiVersion: '2015-08-04',
+    });
   }
 
   async execute(id, data) {
@@ -17,12 +17,12 @@ class PublishPost extends Operation {
     try {
       const post = await this.SavePost.save(id, data);
 
-      // await this.firehose.putRecord({
-      //   DeliveryStreamName: 'AddPost-cms',
-      //   Record: {
-      //     Data: JSON.stringify(PublistPostStreams(post.toJSON())),
-      //   },
-      // }).promise();
+      await this.firehose.putRecord({
+        DeliveryStreamName: 'AddPost-cms',
+        Record: {
+          Data: JSON.stringify(PublistPostStreams(post.toJSON())),
+        },
+      }).promise();
 
       this.emit(SUCCESS, { id });
     } catch (error) {
