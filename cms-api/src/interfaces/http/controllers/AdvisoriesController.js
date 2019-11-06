c204fab3d2d38b271c5f643185a8474b2a393b04
Waleed Almogbel <walmogbel@stratpoint.com>
2019-11-06 17:50:27 +0800

    validation for advisories

--- cms-api/src/interfaces/http/controllers/AdvisoriesController.js
+++ cms-api/src/interfaces/http/controllers/AdvisoriesController.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 
 const { Router } = require('express');
 const { BaseController } = require('@brewery/core');
@@ -30,6 +31,53 @@ class AdvisoriesController extends BaseController {
       .on(ERROR, next);
 
     operation.execute(req.query);
+  }
+
+  create(req, res, next) {
+    const { operation } = req;
+    const { SUCCESS, ERROR, VALIDATION_ERROR } = operation.events;
+
+    operation
+      .on(SUCCESS, (result) => {
+        res
+          .status(Status.CREATED)
+          .json(result);
+      })
+      .on(VALIDATION_ERROR, (error) => {
+        res.status(Status.BAD_REQUEST).json({
+          message: error.message,
+        });
+      })
+      .on(ERROR, next);
+
+    operation.execute(req.body);
+  }
+
+  update(req, res, next) {
+    const { operation } = req;
+    const {
+      SUCCESS, ERROR, VALIDATION_ERROR, NOT_FOUND,
+    } = operation.events;
+
+    operation
+      .on(SUCCESS, (result) => {
+        res
+          .status(Status.ACCEPTED)
+          .json(result);
+      })
+      .on(VALIDATION_ERROR, (error) => {
+        res.status(Status.BAD_REQUEST).json({
+          message: error.message,
+        });
+      })
+      .on(NOT_FOUND, (error) => {
+        res.status(Status.NOT_FOUND).json({
+          message: error.message,
+        });
+      })
+      .on(ERROR, next);
+
+    operation.execute(Number(req.params.id), req.body);
   }
 }
 
