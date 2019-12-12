57fe47b9642bb1c2885cfbe7a66dfea9e0364fe9
Michael Raagas <mraagas@stratpoint.com>
2019-12-12 17:15:20 +0800

    lock post event with user details and kick lock post websocket event

--- cms-api/src/app/sockets/LockPostSocket.js
+++ cms-api/src/app/sockets/LockPostSocket.js
@@ -1,11 +1,13 @@
-const { Operation } = require('@brewery/core');
 const AWS = require('aws-sdk');
+const Post = require('src/domain/Post');
+const { Operation } = require('../../infra/core/core');
 
 class LockPostSocket extends Operation {
-  constructor({ SocketRepository }) {
+  constructor({ PostRepository, UserRepository }) {
     super();
 
-    this.SocketRepository = SocketRepository;
+    this.PostRepository = PostRepository;
+    this.UserRepository = UserRepository;
     this.socketConnector = new AWS.ApiGatewayManagementApi({
       apiVersion: '2018-11-29',
       endpoint: process.env.WEBSOCKET_API_ENDPOINT,
@@ -13,14 +15,100 @@ class LockPostSocket extends Operation {
   }
 
   async lock(event) {
-    // const { connectionId } = event.requestContext;
-    // const { data } = JSON.parse(event.body);
+    const headers = {
+      'Content-Type': 'text/plain',
+      'Access-Control-Allow-Origin': '*',
+    };
 
-    console.log('lock post');
+    const { connectionId } = event.requestContext;
+    const { data: { id, userId } } = JSON.parse(event.body);
+
+    // validate userId
+    const user = await this.UserRepository.getUserById(userId);
+    if (!user) {
+      return {
+        statusCode: 500,
+        headers,
+        body: 'Invalid user id',
+      };
+    }
+
+    // validate post
+    const post = await this.PostRepository.getById(id);
+    if (!post) {
+      return {
+        statusCode: 500,
+        headers,
+        body: 'Invalid post id',
+      };
+    }
+
+    if (post.isLocked) return;
+
+    const payload = new Post({
+      isLocked: true,
+      lockUser: {
+        connectionId,
+        userId,
+        name: `${user.firstName} ${user.lastName}`,
+      },
+    });
+
+    try {
+      await this.PostRepository.update(id, payload);
+      await this.PostRepository.getPostById(id);
+    } catch (error) {
+      console.log('Lock Post Error', error);
+    }
   }
 
   async kick(event) {
-    console.log('kick locked post');
+    const headers = {
+      'Content-Type': 'text/plain',
+      'Access-Control-Allow-Origin': '*',
+    };
+
+    const { connectionId } = event.requestContext;
+    const { data: { id, userId } } = JSON.parse(event.body);
+
+    // validate userId
+    const user = await this.UserRepository.getUserById(userId);
+    if (!user) {
+      return {
+        statusCode: 500,
+        headers,
+        body: 'Invalid user id',
+      };
+    }
+
+    // validate post
+    const post = await this.PostRepository.getById(id);
+    if (!post) {
+      return {
+        statusCode: 500,
+        headers,
+        body: 'Invalid post id',
+      };
+    }
+
+    if (!post.isLocked) return;
+
+    const payload = new Post({
+      isLocked: true,
+      lockUser: {
+        connectionId,
+        userId,
+        name: `${user.firstName} ${user.lastName}`,
+      },
+    });
+
+    try {
+      await this.PostRepository.update(id, payload);
+      await this.PostRepository.getPostById(id);
+    } catch (error) {
+      console.log('Kick Locked Post Error', error);
+    }
   }
 }
+
 module.exports = LockPostSocket;
