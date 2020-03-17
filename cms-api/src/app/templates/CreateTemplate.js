b326d31a88f4f56f7be852868ff20999a9da2f08
Michael Raagas <michaeljhon.raagas@gmail.com>
2020-03-17 11:37:22 +0800

    template crud

---
+++ cms-api/src/app/templates/CreateTemplate.js
@@ -0,0 +1,19 @@
+const Template = require('src/domain/Post');
+const { Operation } = require('../../infra/core/core');
+
+class CreateTemplate extends Operation {
+  constructor({ TemplateRepository }) {
+    super();
+    this.TemplateRepository = TemplateRepository;
+  }
+
+  async execute(id, data) {
+    const {
+      SUCCESS, ERROR, VALIDATION_ERROR, NOT_FOUND,
+    } = this.events;
+  }
+}
+
+CreateTemplate.setEvents(['SUCCESS', 'ERROR', 'VALIDATION_ERROR', 'NOT_FOUND']);
+
+module.exports = CreateTemplate;
---
+++ cms-api/src/infra/repositories/TemplateRepository.js
@@ -0,0 +1,85 @@
+const Sequelize = require('sequelize');
+const { BaseRepository } = require('../../infra/core/core');
+
+const { Op } = Sequelize;
+
+class TemplateRepository extends BaseRepository {
+  constructor({ TemplateModel }) {
+    super(TemplateModel);
+  }
+
+  buildListArgs(data = {}) {
+    // init fetch arguments
+    const args = {
+      where: {
+        isActive: 1,
+      },
+      limit: 20,
+    };
+
+    let order = [['updatedAt', 'DESC']];
+
+    // set keyword
+    if ('keyword' in data
+      && data.keyword) {
+      if ('ids' in data) {
+        args.where = {
+          id: data.ids,
+        };
+      } else {
+        data.keyword = data.keyword.toLowerCase();
+        args.where = {
+          [Op.or]: [
+            Sequelize.where(
+              Sequelize.fn('lower', Sequelize.col('title')),
+              {
+                [Op.like]: `%${data.keyword}%`,
+              },
+            ),
+            Sequelize.where(
+              Sequelize.fn('lower', Sequelize.col('content')),
+              {
+                [Op.like]: `%${data.keyword}%`,
+              },
+            ),
+          ],
+        };
+      }
+    }
+
+    if ('category' in data) {
+      args.where.category = data.category;
+    }
+
+    // offset
+    if ('offset' in data) {
+      args.offset = Number(data.offset);
+    }
+
+    // limit
+    if ('limit' in data) {
+      args.limit = Number(data.limit);
+    }
+
+    if ('order' in data) {
+      // customized sorting via date
+      order = [['publishedAt', data.order.publishedAt]];
+    }
+
+    args.order = order;
+
+    return args;
+  }
+
+  getTemplates(args) {
+    return this.getAll({
+      ...this.buildListArgs(args)
+    });
+  }
+
+  count(args) {
+    return this.model.count(this.buildListArgs(args));
+  }
+}
+
+module.exports = TemplateRepository;
