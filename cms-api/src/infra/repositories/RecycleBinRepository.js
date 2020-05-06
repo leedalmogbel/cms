643988b779598a88110dd506bb66ed6e5d3c63cb
leed almogbel <leedalmogbel@gmail.com>
2020-05-06 10:05:45 +0800

    revert changes

--- cms-api/src/infra/repositories/RecycleBinRepository.js
+++ cms-api/src/infra/repositories/RecycleBinRepository.js
@@ -135,17 +135,7 @@ class RecycleBinRepository extends BaseRepository {
     try {
       if (typeof ids !== 'number') {
         await Promise.all(
-          posts.map(
-            async (post) => {
-              if (post.type === 'post') {
-                this.restore(post, transaction);
-              }
-
-              if (post.type === 'advisory') {
-                this.restoreAdvisory(post, transaction);
-              }
-            },
-          ),
+          posts.map(async (post) => { this.restore(post, transaction); })
         );
       } else {
         await this.restore(posts, transaction);
