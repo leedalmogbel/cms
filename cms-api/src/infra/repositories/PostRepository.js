3b3687930d457b2c75499f3fad6a7e327e9aae2e
Michael Raagas <michaeljhon.raagas@gmail.com>
2020-06-10 14:12:08 +0800

    revert filtering of multiple dates to query AND

--- cms-api/src/infra/repositories/PostRepository.js
+++ cms-api/src/infra/repositories/PostRepository.js
@@ -153,43 +153,19 @@ class PostRepository extends BaseRepository {
       const { dateFrom, dateTo } = data.date;
 
       if (dateFrom && dateTo) {
-        args.where = {
-          ...args.where,
-          [Op.or]: [{
-            createdAt: {
-              [Op.between]: [
-                new Date(dateFrom).toISOString(),
-                new Date(new Date(dateTo).setHours(24, 0, 0, 0)).toISOString(),
-              ],
-            },
-          }],
-        };
+        // NOTE: reference for future use
+        /* const date = new Date(data.date);
+        const startDate = new Date(date.setHours(0, 0, 0, 0)).toISOString();
+        const endDate = new Date(date.setHours(24, 0, 0, 0)).toISOString(); */
 
-        order = [['createdAt', 'DESC']];
-      }
-    }
-
-    // filter recalled date
-    if ('recalledDate' in data && data.recalledDate) {
-      const { dateFrom, dateTo } = data.recalledDate;
-
-      if (dateFrom && dateTo) {
-        args.where = {
-          ...args.where,
-          [Op.or]: [
-            ...args.where[Op.or] ? args.where[Op.or] : [],
-            {
-              recalledAt: {
-                [Op.between]: [
-                  new Date(dateFrom).toISOString(),
-                  new Date(new Date(dateTo).setHours(24, 0, 0, 0)).toISOString(),
-                ],
-              },
-            },
+        args.where.createdAt = {
+          [Op.between]: [
+            new Date(dateFrom).toISOString(),
+            new Date(new Date(dateTo).setHours(24, 0, 0, 0)).toISOString(),
           ],
         };
 
-        order = [['recalledAt', 'DESC']];
+        order = [['createdAt', 'DESC']];
       }
     }
 
@@ -198,18 +174,10 @@ class PostRepository extends BaseRepository {
       const { dateFrom, dateTo } = data.publishedDate;
 
       if (dateFrom && dateTo) {
-        args.where = {
-          ...args.where,
-          [Op.or]: [
-            ...args.where[Op.or] ? args.where[Op.or] : [],
-            {
-              publishedAt: {
-                [Op.between]: [
-                  new Date(dateFrom).toISOString(),
-                  new Date(new Date(dateTo).setHours(24, 0, 0, 0)).toISOString(),
-                ],
-              },
-            },
+        args.where.publishedAt = {
+          [Op.between]: [
+            new Date(dateFrom).toISOString(),
+            new Date(new Date(dateTo).setHours(24, 0, 0, 0)).toISOString(),
           ],
         };
 
@@ -222,22 +190,30 @@ class PostRepository extends BaseRepository {
       const { dateFrom, dateTo } = data.modifiedDate;
 
       if (dateFrom && dateTo) {
-        args.where = {
-          ...args.where,
-          [Op.or]: [
-            ...args.where[Op.or] ? args.where[Op.or] : [],
-            {
-              updatedAt: {
-                [Op.between]: [
-                  new Date(dateFrom).toISOString(),
-                  new Date(new Date(dateTo).setHours(24, 0, 0, 0)).toISOString(),
-                ],
-              },
-            },
+        args.where.updatedAt = {
+          [Op.between]: [
+            new Date(dateFrom).toISOString(),
+            new Date(new Date(dateTo).setHours(24, 0, 0, 0)).toISOString(),
           ],
         };
 
         order = [['updatedAt', 'DESC']];
+      }
+    }
+
+    // filter recalled date
+    if ('recalledDate' in data && data.recalledDate) {
+      const { dateFrom, dateTo } = data.recalledDate;
+
+      if (dateFrom && dateTo) {
+        args.where.recalledAt = {
+          [Op.between]: [
+            new Date(dateFrom).toISOString(),
+            new Date(new Date(dateTo).setHours(24, 0, 0, 0)).toISOString(),
+          ],
+        };
+
+        order = [['recalledAt', 'DESC']];
       }
     }
 
