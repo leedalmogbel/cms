f30b4d7a91815b98b8132b633158d492285e51da
Michael Raagas <michaeljhon.raagas@gmail.com>
2020-06-17 14:41:52 +0800

    post csv export convert post repository filters to raw query

--- cms-api/src/app/export/ExportAdvisories.js
+++ cms-api/src/app/export/ExportAdvisories.js
@@ -11,7 +11,7 @@ class ExportAdvisories extends Operation {
     console.log('Export Started');
     console.log('export post data', data);
 
-    const { userId, type } = data;
+    const { userId } = data;
     const bucket = process.env.BUCKET_NAME;
     const today = new Date().toISOString().slice(0, 10);
     const filename = `${today}-advisories-${userId}`;
@@ -25,7 +25,7 @@ class ExportAdvisories extends Operation {
       SELECT IFNULL(category, ''), IFNULL(title, ''), IFNULL(content, ''), IFNULL(source, ''), IFNULL(locationAddress, ''), IFNULL(tagsAdded, ''), IFNULL(publishedAt, '')
       FROM advisories
       WHERE status != "initial"
-      ORDER BY updatedAt DESC
+      ORDER BY publishedAt DESC
       INTO OUTFILE S3 "s3://${bucket}/${prefix}/${filename}"
       FIELDS TERMINATED BY ','
       OPTIONALLY ENCLOSED BY '"'
--- cms-api/src/app/export/ExportPosts.js
+++ cms-api/src/app/export/ExportPosts.js
@@ -1,4 +1,5 @@
 const { Operation } = require('../../infra/core/core');
+const { filter } = require('compression');
 
 class ExportPosts extends Operation {
   constructor({ PostRepository }) {
@@ -11,11 +12,45 @@ class ExportPosts extends Operation {
     console.log('Export Started');
     console.log('export post data', data);
 
+    const self = this;
+    data = data || {};
+
+    // convert sequelize filters to raw query
+    async function getRawFilters() {
+      const args = await self.PostRepository.buildListArgs(data);
+
+      // remove default limit
+      if (!('limit' in data) && 'limit' in args) {
+        delete args.limit;
+      }
+
+      return new Promise((resolve) => {
+        self.PostRepository.getAll({
+          ...args,
+          logging: (raw) => {
+            let filterRaw = '';
+            const index = raw.indexOf('WHERE');
+
+            if (index > -1) {
+              filterRaw = raw.substr(index).replace(';', '');
+            }
+
+            resolve(filterRaw);
+          },
+        });
+      });
+    }
+
+    // get raw filters
+    const filters = await getRawFilters();
+
+    // format csv filename
     const { userId } = data;
     const bucket = process.env.BUCKET_NAME;
     const today = new Date().toISOString().slice(0, 10);
     const filename = `${today}-posts-${userId}`;
 
+    // set prefix
     const env = process.env.NODE_ENV;
     const prefix = env !== 'local' ? `csv-export-${env}` : 'csv-export-dev';
 
@@ -23,9 +58,8 @@ class ExportPosts extends Operation {
       SELECT "postId", "contributors", "category", "title", "content", "locationAddress", "tagsOriginal", "tagsRetained", "tagsRemoved", "tagsAdded", "status", "publishedAt", "scheduledAt", "expiredAt", "createdAt", "updatedAt"
       UNION ALL
       SELECT postId, contributors, IFNULL(category, ''), IFNULL(title, ''), IFNULL(content, ''), IFNULL(locationAddress, ''), IFNULL(tagsOriginal, ''), IFNULL(tagsRetained, ''), IFNULL(tagsRemoved, ''), IFNULL(tagsAdded, ''), IFNULL(status, ''), IFNULL(publishedAt, ''), IFNULL(scheduledAt, ''), IFNULL(expiredAt, ''), createdAt, updatedAt
-      FROM posts
-      WHERE status != "initial"
-      ORDER BY updatedAt DESC
+      FROM posts as PostModel
+      ${filters}
       INTO OUTFILE S3 "s3://${bucket}/${prefix}/${filename}"
       FIELDS TERMINATED BY ','
       OPTIONALLY ENCLOSED BY '"'
